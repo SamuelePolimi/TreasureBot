7b4fb738faa829b248cf465325e32c86448313fa
Yan Ni <niyan.ny@pku.edu.cn>
2017-05-16 05:16:52 -0700

    modify lstm_example.py on dataset path

--- examples/lstm_example.py
+++ examples/lstm_example.py
@@ -7,8 +7,8 @@ from core.bot import Bot
 
 print "Loading dataset"
 
-train_set = np.load("dataset/forex/ds2/train_set.npy")
-validation_set = np.load("dataset/forex/ds2/validation_set.npy")
+train_set = np.load("dataset/forex/ds1/train_set.npy")
+validation_set = np.load("dataset/forex/ds1/validation_set.npy")
 
 #very short sequence for debugging purpose
 
